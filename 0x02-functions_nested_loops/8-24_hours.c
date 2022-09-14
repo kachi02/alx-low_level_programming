dcffea5b6b755280dd853d25c3a002a4290c47a3
kachi02 <02ibekwe@gmail.com>
2022-09-13 17:06:51 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,13 +15,13 @@ void jack_bauer(void)
 	{
 		while (minutes <= 59)
 		{
-			mins_remainder = minutes % 10;
-			hours_remainder = hours % 10;
+			mins_reminder = minutes % 10;
+			hours_reminder = hours % 10;
 			_putchar(hours / 10 + '0');
-			_putchar(hours_remainder + '0');
+			_putchar(hours_reminder + '0');
 			_putchar(':');
 			_putchar(minutes / 10 + '0');
-			_putchar(mins_remainder + '0');
+			_putchar(mins_reminder + '0');
 			minutes++;
 			_putchar('\n');
 		}
