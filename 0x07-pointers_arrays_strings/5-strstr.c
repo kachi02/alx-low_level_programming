954dc3684a97d68d3b2371b0c1579bd1c7b17857
kachi02 <02ibekwe@gmail.com>
2022-09-27 14:47:42 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strstr - Entry point
+ * _strstr - entry point
  * @haystack: input
  * @needle: input
  * Return: Always 0
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 		char *one = haystack;
 		char *two = needle;
 
-		while (*one == *two && *two == != '\0')
+		while (*one == *two && *two != '\0')
 		{
 			one++;
 			two++;
