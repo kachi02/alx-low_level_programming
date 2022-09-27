93a4da6bf353f7c44232ba7ca13527dca4fc62df
kachi02 <02ibekwe@gmail.com>
2022-09-27 09:21:43 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *memcpy(char *dest, char *src, unsigned int n);
@@ -11,4 +16,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
-#endif
+#endif /*MAIN_H*/
