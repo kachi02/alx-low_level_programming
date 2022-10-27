bcb7deace7aea856b021942e9f08c688a61d7e8e
kachi02 <02ibekwe@gmail.com>
2022-10-27 16:31:11 -0700

    4-clear_bits.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,8 +5,8 @@
  * @n: a pointer to the bit.
  * @index: the index to set the value at - indices start at 0.
  *
- * Return: if error, -1.
- *         otherwise, 1.
+ * Return: if error occurs - -1.
+ *         otherwise - 1.
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits needed to be flipped to get from
+ *             one number to another.
